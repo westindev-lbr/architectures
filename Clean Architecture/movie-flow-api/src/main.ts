f24002c90ec4a7ff511d8c44dcdbdae334f9417a
Axel Labarre <axel.labarre@gmail.com>
2024-10-28 22:38:30 +0100

    ➕ : replace the Express framework with Fastify

--- Clean Architecture/movie-flow-api/src/main.ts
+++ Clean Architecture/movie-flow-api/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
+import { log } from 'console';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule);
+  await app.listen({ port: 3000 });
+  app.useLogger(['log', 'error', 'debug', 'verbose']);
+  log(`Application is running on : ${await app.getUrl()}`);
 }
 bootstrap();
