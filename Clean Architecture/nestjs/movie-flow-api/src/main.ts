18baf9e2c2d0e62991cf1b90a56b6479bfbafaaf
Axel Labarre <axel.labarre@gmail.com>
2024-11-09 12:43:20 +0100

    chore : move the project into nestjs dir

--- Clean Architecture/nestjs/movie-flow-api/src/main.ts
+++ Clean Architecture/nestjs/movie-flow-api/src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestFastifyApplication } from '@nestjs/platform-fastify';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { log } from 'console';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule);
-  await app.listen({ port: 3000 });
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  await app.listen(3000, '0.0.0.0');
   app.useLogger(['log', 'error', 'debug', 'verbose']);
   log(`Application is running on : ${await app.getUrl()}`);
 }
